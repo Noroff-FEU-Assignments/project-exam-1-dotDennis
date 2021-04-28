f44c0424c6816155e01469c990422094e961a38a
Dennis <71315485+dotDennis@users.noreply.github.com>
2021-04-28 18:45:39 +0200

    Ready for API
    need to change up some stuff in the carousel script, but mostly ready

--- js/carousel.js
+++ js/carousel.js
@@ -1,16 +1,57 @@
 const carouselContainer = document.querySelector(".carousel__items");
-const items = Array.from(carouselContainer.children);
 const nextButton = document.querySelector(".right");
 const previousButton = document.querySelector(".left");
 
-const itemWidth = items[0].getBoundingClientRect().width;
+// fetch data from api and create carousel html
+const url = "https://dennisl.no/blogAPI/wp-json/wp/v2/posts?_embed";
 
-console.log(itemWidth);
+async function getPosts() {
+  try {
+    // await response then await json
+    const response = await fetch(url);
+    const json = await response.json();
+
+    for (let i = 0; i < json.length; i++) {
+      // declarations
+      const title = json[i].title.rendered;
+      const featuredImage = json[i]._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
+      const author = json[i]._embedded.author[0].name;
+      const date = json[i].date;
+      // const imageTest = json[i]._embedded["wp:attachment"];
+
+      console.log(title);
+      console.log(featuredImage);
+      console.log(author);
+      console.log(date);
+
+      if (title) {
+        carouselContainer.innerHTML += `<div class="carousel__post post-${[i]} current-item">
+              <img class="item__image" src="${featuredImage}" alt="${title}" />
+              <h2>${title}</h2>
+              <p>By <a href="${author}">${author}</a> / ${date}</p>
+            </div>`;
+      }
+    }
+  } catch (error) {
+    // if there's an error - display error to user
+    // featuredContainer.innerHTML = createError(error);
+    console.log(error);
+  } finally {
+    // remove loader
+    // loader.classList.remove("loader");
+    console.log("done");
+    const items = Array.from(carouselContainer.children);
+
+    const itemWidth = items[0].getBoundingClientRect().width;
+    for (let i = 0; i < items.length; i++) {
+      items[i].style.left = itemWidth * i + "px";
+    }
+  }
+}
+
+getPosts();
 
 // Arrange items & set position next to eachother
-for (let i = 0; i < items.length; i++) {
-  items[i].style.left = itemWidth * i + "px";
-}
 
 function moveToSlide(container, currentItem, targetItem) {
   if (!targetItem) {
