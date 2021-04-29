9f35d1a60b004dd3202d9570236c51a3896332c6
Dennis <71315485+dotDennis@users.noreply.github.com>
2021-04-29 18:45:29 +0200

    Updated carousel script, need to make it a endless loop next
    job for another time =)

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,4 @@
-const carouselContainer = document.querySelector(".carousel__items");
-const nextButton = document.querySelector(".right");
-const previousButton = document.querySelector(".left");
+const carouselTrack = document.querySelector(".track");
 
 // fetch data from api and create carousel html
 const url = "https://dennisl.no/blogAPI/wp-json/wp/v2/posts?_embed";
@@ -12,24 +10,58 @@ async function getPosts() {
     const json = await response.json();
 
     for (let i = 0; i < json.length; i++) {
-      // declarations
+      // declare json data that will be used for creating the html
       const title = json[i].title.rendered;
-      const featuredImage = json[i]._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
+      const featuredImage = json[i]._embedded["wp:featuredmedia"][0].source_url;
       const author = json[i]._embedded.author[0].name;
-      const date = json[i].date;
-      // const imageTest = json[i]._embedded["wp:attachment"];
 
-      console.log(title);
-      console.log(featuredImage);
-      console.log(author);
-      console.log(date);
+      // format the date
+      const date = new Date(json[i].date);
+      const format = { day: "numeric", month: "numeric", year: "numeric" };
+      const dateFormatted = date.toLocaleString("en-GB", format).replace(/\//g, ".");
 
-      if (title) {
-        carouselContainer.innerHTML += `<div class="carousel__post post-${[i]} current-item">
-              <img class="item__image" src="${featuredImage}" alt="${title}" />
-              <h2>${title}</h2>
-              <p>By <a href="${author}">${author}</a> / ${date}</p>
-            </div>`;
+      if (i === 0) {
+        carouselTrack.innerHTML += `
+          <div class="post-container">
+            <div id="first" class="post current-item">
+              <div class="img-container">
+                <img src="${featuredImage}" alt="${title}"/>
+              </div>
+              <div class="post-info">
+                <h2>${title}</h2>
+                <p>By <a href="${author}">${author}</a> / ${dateFormatted}</p>
+              </div>
+            </div>
+          </div>`;
+      } else {
+        if (i === json.length - 1) {
+          carouselTrack.innerHTML += `
+          <div class="post-container">
+            <div id="last" class="post">
+              <div class="img-container">
+                <img src="${featuredImage}" alt="${title}"/>
+              </div>
+              <div class="post-info">
+                <h2>${title}</h2>
+                <p>By <a href="${author}">${author}</a> / ${dateFormatted}</p>
+              </div>
+            </div>
+          </div>`;
+        }
+        if (i < 0 || i !== json.length - 1) {
+          carouselTrack.innerHTML += `
+          <div class="post-container">
+            <div class="post">
+              <div class="img-container">
+                <img src="${featuredImage}" alt="${title}"/>
+              </div>              
+              <div class="post-info">
+                <h2>${title}</h2>
+                <p>By <a href="${author}">${author}</a> / ${dateFormatted}</p>
+              </div>
+            </div>
+          </div>`;
+        }
       }
     }
   } catch (error) {
@@ -40,41 +72,56 @@ async function getPosts() {
     // remove loader
     // loader.classList.remove("loader");
     console.log("done");
-    const items = Array.from(carouselContainer.children);
+    const postContainer = document.querySelectorAll(".post-container");
+    const postWidth = postContainer[4].offsetWidth;
 
-    const itemWidth = items[0].getBoundingClientRect().width;
-    for (let i = 0; i < items.length; i++) {
-      items[i].style.left = itemWidth * i + "px";
-    }
+    const nextButton = document.querySelector("#next");
+    const prevButton = document.querySelector("#prev");
+
+    let clickCount = 1;
+
+    nextButton.addEventListener("click", function () {
+      if (clickCount === postContainer.length) {
+        return;
+      } else {
+        carouselTrack.style.transform = `translateX(-${postWidth * clickCount}px)`;
+        clickCount++;
+      }
+
+      console.log(clickCount);
+      console.log(postContainer.length);
+    });
+
+    prevButton.addEventListener("click", function () {
+      carouselTrack.style.transform = `translateX(-0px)`;
+      console.log("bop-back");
+      clickCount = 2;
+    });
   }
 }
 
 getPosts();
 
-// Arrange items & set position next to eachother
+// carousel track has been decalared at start of document.
 
-function moveToSlide(container, currentItem, targetItem) {
-  if (!targetItem) {
-    // if (targetItem.getBoundingClientRect().width > 500) Implement wordpress blog posts api to fetch if ID = 1, or if ID > apiJSON.length
-  } else {
-    container.style.transform = "translateX(-" + targetItem.style.left + ")";
-    currentItem.classList.remove("current-item");
-    targetItem.classList.add("current-item");
-  }
-}
+// const carouselWidth = document.querySelector(".carousel-container").offsetWidth;
 
-// when click previous (left button), move slides towards left
-previousButton.addEventListener("click", function (event) {
-  const currentItem = carouselContainer.querySelector(".current-item");
-  const previousItem = currentItem.previousElementSibling;
+// console.log(document.querySelector(".carousel-container"));
 
-  moveToSlide(carouselContainer, currentItem, previousItem);
-});
+// console.log(carouselWidth);
 
 // when click next (right button), move slides towards right
-nextButton.addEventListener("click", function (event) {
-  const currentItem = carouselContainer.querySelector(".current-item");
-  const nextItem = currentItem.nextElementSibling;
+// nextButton.addEventListener("click", function (event) {
+//   const currentItem = carouselTrack.querySelector(".current-item");
+//   const nextItem = currentItem.nextElementSibling;
 
-  moveToSlide(carouselContainer, currentItem, nextItem);
-});
+//   moveToSlide(carouselTrack, currentItem, nextItem);
+// });
+
+// // when click previous (left button), move slides towards left
+// prevButton.addEventListener("click", function (event) {
+//   const currentItem = carouselTrack.querySelector(".current-item");
+//   const previousItem = currentItem.previousElementSibling;
+
+//   moveToSlide(carouselTrack, currentItem, previousItem);
+// });
